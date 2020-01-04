03bd760e19489a9ed7e4de696968fe0db77640f7
zhenjess <jessicazhen8@gmail.com>
2020-01-04 01:25:36 -0800

    Allow user to create and show cart

--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -1,12 +1,21 @@
 class Api::CartsController < ApplicationController
   def create
     @cart = Cart.new(user_id: params[:user_id]) #a user's cart
+    
+    #if cart has orders, show full cart else show empty cart
+    #show user's created cart or render error if user has no cart
+    render 'api/cart/show'
+  end
 
-    if @cart.save 
-        @user = current_user
-        @user.cart
+  def show #show user's cart shoe order if user has a cart
+    @cart = Cart.where(id: params[:id]).where(user_id: params[:user_id])
+                .includes(:orders, :shoes)
+    @cart = @cart[0]
+
+    if @cart
+        render 'api/cart/show'
     else
-        render json: @cart.errors.full_messages
+        render json: { errors: 'Not Available'}, status: 400
     end
   end
 end
