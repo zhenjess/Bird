ec6484a9bf02a76306ae73c39538552303d22af9
zhenjess <jessicazhen8@gmail.com>
2020-01-04 14:45:50 -0800

    Combine modal reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -8,14 +8,12 @@ import ordersReducer from './orders_reducer';
 
 import cartsReducer from './carts_reducer';
 
-import modalReducer from './modal_reducer';
 
 const entitiesReducer = combineReducers({
     users: usersReducer,
     shoes: shoesReducer,
     orders: ordersReducer,
-    carts: cartsReducer, 
-    modal: modalReducer
+    carts: cartsReducer
 });
 
 export default entitiesReducer; //represents data from db and manages it
