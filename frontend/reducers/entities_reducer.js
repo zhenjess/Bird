82c074807280358d0f48b739b74ae8d35a8ae0a5
zhenjess <jessicazhen8@gmail.com>
2020-01-04 14:41:10 -0800

    Entities combine reducers

---
+++ frontend/reducers/entities_reducer.js
@@ -0,0 +1,19 @@
+import { combineReducers } from "redux";
+
+import usersReducer from './users_reducer';
+
+import shoesReducer from './shoes_reducer';
+
+import ordersReducer from './orders_reducer';
+
+import cartsReducer from './carts_reducer';
+
+const entitiesReducer = combineReducers({
+    users: usersReducer,
+    shoes: shoesReducer,
+    orders: ordersReducer,
+    carts: cartsReducer
+});
+
+export default entitiesReducer; //represents data from db and manages it
+//then delegates tasks to each reducer
