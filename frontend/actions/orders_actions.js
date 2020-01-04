03a55c7823aef6fa6733f543242f2b9c45dfa872
zhenjess <jessicazhen8@gmail.com>
2020-01-04 15:47:21 -0800

    Create route util for user auth

--- frontend/actions/orders_actions.js
+++ frontend/actions/orders_actions.js
@@ -38,3 +38,20 @@ export const updateOrder = (user, order) => dispatch => {
 //         .then(response => dispatch(removeOrder(response)));
 // }
 
+import { receiveShoeItems } from "../util/items_api_util";
+
+export const RECEIVE_ITEMS = "RECEIVE_ITEMS";
+
+const fetchShoeItems = items => {
+    return ({
+        type: RECEIVE_ITEMS,
+        items: items
+    });
+};
+
+export const fetchItems = (id) => (dispatch) => (
+    receiveShoeItems(id)
+    .then( items => dispatch(fetchShoeItems(items)))
+);
+
+
