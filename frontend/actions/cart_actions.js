59c10e38b2d45ef69b2620dda0a32820b55640bc
zhenjess <jessicazhen8@gmail.com>
2020-01-04 13:06:47 -0800

    Create cart action creators adding shoe orders and removing shoe orders to cart

---
+++ frontend/actions/cart_actions.js
@@ -0,0 +1,45 @@
+import * as APIUtil from '../util/cart_api_util';
+
+//action creators
+export const POST_TO_CART = "POST_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+
+const postToCart = (payload) => {
+    return ({
+        type: POST_TO_CART,
+        order: payload
+    });
+};
+
+const removeCartOrder = (id, shoeSize) => {
+    return ({
+        type: REMOVE_FROM_CART,
+        id,
+        shoeSize
+    });
+};
+
+function receiveOrders(shoe, size) {
+    let state = Object.assign({}, shoe, { size });
+    let key = `${shoe.id}-${size}`;
+    return ({
+        [key]: state
+    });
+}
+
+export const addToCart = (order, size) => dispatch => {
+    const payload = receiveOrders(order, size);
+    function _func(payload) {
+        return dispatch(postToCart(payload));
+    }
+    return _func(payload);
+};
+
+
+
+export const removeFromCart = (id, size) => dispatch => {
+    function _func(id, size) {
+        return dispatch(removeCartOrder(id, size));
+    }
+    return _func(id, size);
+}
