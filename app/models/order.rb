08162f8f97dd83db6133aafe28c4b21901c995e0
App Academy Student <student@appacademy.io>
2020-01-03 17:52:32 -0800

    Create orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,15 @@
 class Order < ApplicationRecord
   validates :size, :quantity, presence: true
-  
+
   belongs_to :shoe
 
   belongs_to :cart
 
+  has_one_attached :photo
+
+  def self.find_by_gender(search_id)
+    Order.joins(:shoe)
+    .where(shoes: {gender: search_id})
+    .select("orders.id", :shoe_id, :cart_id, :size, :quantity, :model, :gender, :color, :material, :price)
+  end
 end
--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,7 +1,13 @@
 class Shoe < ApplicationRecord
-  validates :model, :gender, :color, :material, :size, :price, presence: true
+  TYPES = [
+        'Tree',
+        'Wool'
+    ].sort.freeze
+
+  validates :model, :gender, :color, :material, :size, :price, presence: true, inclusion: { in: TYPES }
 
   has_many :orders
   
   has_many_attached :photos
+
 end
