48a3cd38a3cbfd300ec9617d007fe9310eac1ea9
zhenjess <jessicazhen8@gmail.com>
2020-01-04 01:13:43 -0800

    Create carts controller

--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -1,3 +1,12 @@
 class Api::CartsController < ApplicationController
-  
+  def create
+    @cart = Cart.new(user_id: params[:user_id]) #a user's cart
+
+    if @cart.save 
+        @user = current_user
+        @user.cart
+    else
+        render json: @cart.errors.full_messages
+    end
+  end
 end
