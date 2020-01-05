890b09c0a96903650b0d55fad66df5231fe60e86
zhenjess <jessicazhen8@gmail.com>
2020-01-04 16:14:17 -0800

    Add routes to order shoe and put shoe in cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+end
+
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "static_pages#root"
+
+  namespace :api, defaults: {format: :json} do
+    resources :orders, only: [:create, :show, :edit]
+    resources :users, only: [:create]
+    resources :carts, only: [:create, :edit, :show, :destroy]
+    resources :shoes,  only: [:index, :show]
+    resource :session, only: [:create, :destroy]
+  end
+
+  
 end
